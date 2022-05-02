564b15784ab2e722d57c8d39378ddeff2f9ade4e
bedelturker <bedel.turker@muni.com.tr>
2022-05-02 17:48:59 +0300

    as combinining styles not working, changing to custom style only

--- src/ScrollPage.tsx
+++ src/ScrollPage.tsx
@@ -4,25 +4,24 @@ import { ScrollPageContext, ScrollContainerContext } from "./ScrollContext";
 interface ScrollPageProps {
   children: React.ReactNode;
   page: number;
-  debugBorder?: boolean;
   customStyle?: {}
 }
 
 const ScrollPage = (props: ScrollPageProps) => {
-  const { children, page, debugBorder = false, customStyle } = props;
+  const { children, page, customStyle } = props;
   const { viewportHeight } = useContext(ScrollContainerContext);
-  const style: React.CSSProperties = {
-    margin: 0,
-    padding: 0,
-    height: viewportHeight,
-    position: "relative",
-    boxSizing: "border-box",
-    scrollSnapAlign: "center",
-    overflow: "hidden",
-    ...(debugBorder ? { border: "1px solid red" } : {}),
-  };
+  // const style: React.CSSProperties = {
+  //   margin: 0,
+  //   padding: 0,
+  //   height: viewportHeight,
+  //   position: "relative",
+  //   boxSizing: "border-box",
+  //   scrollSnapAlign: "center",
+  //   overflow: "hidden",
+  //   ...(debugBorder ? { border: "1px solid red" } : {}),
+  // };
   return (
-    <div style={{...style, ...customStyle}}>
+    <div style={customStyle}>
       <ScrollPageContext.Provider value={{ page }}>
         {children}
       </ScrollPageContext.Provider>
