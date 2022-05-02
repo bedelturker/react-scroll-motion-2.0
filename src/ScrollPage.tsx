95691db687b50abcc050da449f1280ae672adf13
bedelturker <bedel.turker@muni.com.tr>
2022-05-02 17:53:03 +0300

    reverted to spread operator

--- src/ScrollPage.tsx
+++ src/ScrollPage.tsx
@@ -4,24 +4,25 @@ import { ScrollPageContext, ScrollContainerContext } from "./ScrollContext";
 interface ScrollPageProps {
   children: React.ReactNode;
   page: number;
+  debugBorder?: boolean;
   customStyle?: {}
 }
 
 const ScrollPage = (props: ScrollPageProps) => {
-  const { children, page, customStyle } = props;
+  const { children, page, debugBorder = false, customStyle } = props;
   const { viewportHeight } = useContext(ScrollContainerContext);
-  // const style: React.CSSProperties = {
-  //   margin: 0,
-  //   padding: 0,
-  //   height: viewportHeight,
-  //   position: "relative",
-  //   boxSizing: "border-box",
-  //   scrollSnapAlign: "center",
-  //   overflow: "hidden",
-  //   ...(debugBorder ? { border: "1px solid red" } : {}),
-  // };
+  const style: React.CSSProperties = {
+    margin: 0,
+    padding: 0,
+    height: viewportHeight,
+    position: "relative",
+    boxSizing: "border-box",
+    scrollSnapAlign: "center",
+    overflow: "hidden",
+    ...(debugBorder ? { border: "1px solid red" } : {}),
+  };
   return (
-    <div style={customStyle}>
+    <div style={{...style, ...customStyle}}>
       <ScrollPageContext.Provider value={{ page }}>
         {children}
       </ScrollPageContext.Provider>
